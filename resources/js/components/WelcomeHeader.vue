d3969be8a6a14ee4f27deeb1cf111bba239ad14c
Nathan Lehman <nlehman@dlcouch.com>
2019-06-20 12:58:24 -0400

    Hide Blog link for now. Fix Contact button.

--- resources/js/components/WelcomeHeader.vue
+++ resources/js/components/WelcomeHeader.vue
@@ -2,43 +2,48 @@
   <div class="shadow-wrap">
     <header class="px-4 header-gradient flex flex-col text-blue-100 items-center">
       <nav class="max-w-6xl container mx-auto flex justify-between items-center">
-        <svg class="w-16 h-16" viewBox="0 0 663 481" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M0 158L140 0V319L0 481V158Z" fill="url(#paint0_linear)"/>
-          <path d="M280 158L420 0V319L280 481V158Z" fill="url(#paint1_linear)"/>
-          <path d="M662.25 322L420 319L280 481H522.25L662.25 322Z" fill="url(#paint2_linear)"/>
-          <path d="M280 481L0 158L140 0L420 319L280 481Z" fill="url(#paint3_linear)"/>
-          <defs>
-            <linearGradient id="paint0_linear" x1="1.77324e-05" y1="-25" x2="295.541" y2="93.6853"
-                            gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" stop-opacity="0.5"/>
-              <stop offset="1" stop-color="white"/>
-            </linearGradient>
-            <linearGradient id="paint1_linear" x1="280" y1="-25" x2="575.541" y2="93.6853"
-                            gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" stop-opacity="0.5"/>
-              <stop offset="1" stop-color="white"/>
-            </linearGradient>
-            <linearGradient id="paint2_linear" x1="280" y1="310.58" x2="360.79" y2="573.598"
-                            gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" stop-opacity="0.5"/>
-              <stop offset="1" stop-color="white"/>
-            </linearGradient>
-            <linearGradient id="paint3_linear" x1="5.31971e-05" y1="-25" x2="420" y2="481"
-                            gradientUnits="userSpaceOnUse">
-              <stop stop-color="white" stop-opacity="0.5"/>
-              <stop offset="1" stop-color="white"/>
-            </linearGradient>
-          </defs>
-        </svg>
+        <a href="/">
+          <svg class="w-16 h-16" viewBox="0 0 663 481" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 158L140 0V319L0 481V158Z" fill="url(#paint0_linear)"/>
+            <path d="M280 158L420 0V319L280 481V158Z" fill="url(#paint1_linear)"/>
+            <path d="M662.25 322L420 319L280 481H522.25L662.25 322Z" fill="url(#paint2_linear)"/>
+            <path d="M280 481L0 158L140 0L420 319L280 481Z" fill="url(#paint3_linear)"/>
+            <defs>
+              <linearGradient id="paint0_linear" x1="1.77324e-05" y1="-25" x2="295.541" y2="93.6853"
+                              gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.5"/>
+                <stop offset="1" stop-color="white"/>
+              </linearGradient>
+              <linearGradient id="paint1_linear" x1="280" y1="-25" x2="575.541" y2="93.6853"
+                              gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.5"/>
+                <stop offset="1" stop-color="white"/>
+              </linearGradient>
+              <linearGradient id="paint2_linear" x1="280" y1="310.58" x2="360.79" y2="573.598"
+                              gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.5"/>
+                <stop offset="1" stop-color="white"/>
+              </linearGradient>
+              <linearGradient id="paint3_linear" x1="5.31971e-05" y1="-25" x2="420" y2="481"
+                              gradientUnits="userSpaceOnUse">
+                <stop stop-color="white" stop-opacity="0.5"/>
+                <stop offset="1" stop-color="white"/>
+              </linearGradient>
+            </defs>
+          </svg>
+        </a>
         <div>
-          <a href="/blog" class="text-blue-100 no-underline hover:underline font-medium">Blog</a>
+          <!--          <a href="/blog" class="text-blue-100 no-underline hover:underline font-medium">Blog</a>-->
           <a href="#contactForm" class="text-blue-100 no-underline hover:underline font-medium pl-4">Contact</a>
         </div>
       </nav>
       <div class="font-display font-black text-4xl sm:text-6xl pt-10 pb-6">Nathan Lehman</div>
       <div class="pb-10">Web application developer</div>
       <div class="pt-8 pb-40">
-        <button class="text-sm sm:text-lg font-medium rounded-full border-2 px-6 py-4 hover:bg-blue-100-25">
+        <button
+                type="button"
+                v-on:click="goToContact()"
+                class="text-sm sm:text-lg font-medium rounded-full border-2 px-6 py-4 hover:bg-blue-100-25 focus:outline-none focus:shadow-outline">
           Let's build something together
         </button>
       </div>
@@ -47,7 +52,13 @@
 </template>
 
 <script>
-  export default {}
+  export default {
+    methods: {
+      goToContact() {
+        window.location = '#contactForm'
+      }
+    },
+  }
 </script>
 
 <style>
