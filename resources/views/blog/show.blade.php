d5d2fea0326749e556b1ac81e7cad757da7c57b4
Nathan Lehman <nlehman@dlcouch.com>
2019-06-24 15:54:45 -0400

    Tailwind purgecss issues

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -24,7 +24,7 @@
     <div class="container mx-auto px-5 lg:max-w-4xl mt-48">
         <div class="leading-loose text-base flex flex-col justify-center items-center post-body">
 
-            <h1 class="text-5xl">{{ $post->title }}</h1>
+            <h1 class="text-3xl sm:text-5xl">{{ $post->title }}</h1>
             <p class="text-light uppercase mb-10 block">
                 Posted on {{ $post->publish_date->format('F jS, Y')}}
             </p>
